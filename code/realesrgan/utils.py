0398a2535478818873af3f267d1b41c1e7a62aa0
Arnaud <arnaud.gucciardi@toelt.ai>
2025-05-06 19:34:23 +0200

    gan fix

--- code/realesrgan/utils.py
+++ code/realesrgan/utils.py
@@ -61,7 +61,7 @@ class RealESRGANer():
             if model_path.startswith('https://'):
                 model_path = load_file_from_url(
                     url=model_path, model_dir=os.path.join(ROOT_DIR, 'weights'), progress=True, file_name=None)
-            loadnet = torch.load(model_path, map_location=torch.device('cpu'), weights_only=True)
+            loadnet = torch.load(model_path, map_location=torch.device('cpu'))#, weights_only=True)
 
         # prefer to use params_ema
         if 'params_ema' in loadnet:
@@ -80,8 +80,8 @@ class RealESRGANer():
 
         ``Paper: Deep Network Interpolation for Continuous Imagery Effect Transition``
         """
-        net_a = torch.load(net_a, map_location=torch.device(loc), weights_only = True)
-        net_b = torch.load(net_b, map_location=torch.device(loc), weights_only = True)
+        net_a = torch.load(net_a, map_location=torch.device(loc))#, weights_only = True)
+        net_b = torch.load(net_b, map_location=torch.device(loc))#, weights_only = True)
         for k, v_a in net_a[key].items():
             net_a[key][k] = dni_weight[0] * v_a + dni_weight[1] * net_b[key][k]
         return net_a
